484417da71645787db30cbf31f5c45c63b9c60ee
Tayná <taynasg@ciandt.com>
2023-01-19 09:34:11 -0300

    Ciando a estrutura do programa

---
+++ Pessoa.java
@@ -0,0 +1,55 @@
+public class Pessoa {
+
+    private int idade;
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public void caucularIdade(){
+
+    }
+
+    public void informaIdade(){
+
+    }
+
+    public void informaNome(){
+
+    }
+    public void ajustaDataDeNascimento(){
+
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+}
---
+++ TestaPessoa.java
@@ -0,0 +1,15 @@
+public class TestaPessoa {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        System.out.println("=================================================");
+        System.out.println("              CAUCULO DE IDADE");
+        System.out.println("=================================================");
+        System.out.println();
+
+
+
+    }
+
+
+}
